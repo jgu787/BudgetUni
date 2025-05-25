3c68cf7d22933be243e91109c32597fbe32d700f
JiaminGu <jiamingu787@gmail.com>
2025-05-24 22:30:55 -0700

    Added a HomeView with IncomeVsExpenseGraph and SpendingHabitsGraph

---
+++ BudgetUni/IncomeVsExpenseGraphView.swift
@@ -0,0 +1,18 @@
+//
+//  IncomeVsExpenseGraphView.swift
+//  BudgetUni
+//
+//  Created by Jiamin Gu on 2025-05-24.
+//
+
+import SwiftUI
+
+struct IncomeVsExpenseGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IncomeVsExpenseGraphView()
+}
---
+++ BudgetUni/SpendingHabitsGraphView.swift
@@ -0,0 +1,18 @@
+//
+//  SpendingHabitsGraphView.swift
+//  BudgetUni
+//
+//  Created by Jiamin Gu on 2025-05-24.
+//
+
+import SwiftUI
+
+struct SpendingHabitsGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpendingHabitsGraphView()
+}
